5387f5546e96eb74daf08ec1f6a753b55e565247
zhe.gong@maitang001.com <zhe.gong@maitang001.com>
2021-05-21 19:57:05 +0800

    filters init

--- src/Entities/QueryFilters.php
+++ src/Entities/QueryFilters.php
@@ -3,7 +3,6 @@
 
 namespace mdao\QueryOrm\Entities;
 
-
 use InvalidArgumentException;
 
 class QueryFilters implements \JsonSerializable, \ArrayAccess
@@ -27,7 +26,7 @@ class QueryFilters implements \JsonSerializable, \ArrayAccess
      */
     public function __construct($data = [])
     {
-        $this->data =array_keys($data);
+        $this->filters($data);
         $this->initialize();
     }
 
@@ -69,9 +68,9 @@ class QueryFilters implements \JsonSerializable, \ArrayAccess
      * @param array $items
      * @return $this
      */
-    public function filters(array $items=[]): self
+    public function filters(array $items = []): self
     {
-        foreach ($items as  $filter) {
+        foreach ($items as $filter) {
             foreach ($filter as $value) {
                 list($field, $operator, $value) = $value;
                 $this->data[$field] = new QueryFilter($field, $operator, $value);
@@ -81,13 +80,23 @@ class QueryFilters implements \JsonSerializable, \ArrayAccess
     }
 
     /**
+     * @param array $items
+     * @return $this
+     */
+    public static function createFilters(array $items = []): self
+    {
+        return new self($items);
+    }
+
+
+    /**
      * 获取对象原始数据 如果不存在指定字段返回false
      * @access public
      * @param string $name 字段名 留空获取全部
      * @return mixed
      * @throws InvalidArgumentException
      */
-    public function getData($name = null)
+    public function getData($name = null): QueryFilter
     {
         if (is_null($name)) {
             return $this->data;
@@ -97,54 +106,6 @@ class QueryFilters implements \JsonSerializable, \ArrayAccess
             throw new InvalidArgumentException('property not exists:' . $this->class . '->' . $name);
         }
     }
-
-    /**
-     * 修改器 设置数据对象值
-     * @access public
-     * @param string $name 属性名
-     * @param mixed $value 属性值
-     * @param array $data 数据
-     * @return $this
-     */
-    public function setAttr($name, $value, $data = [])
-    {
-        if (is_null($value) && $this->autoWriteTimestamp && in_array($name, [$this->createTime, $this->updateTime])) {
-            // 自动写入的时间戳字段
-            $value = $this->autoWriteTimestamp($name);
-        } else {
-            // 检测修改器
-            $method = 'set' . Loader::parseName($name, 1) . 'Attr';
-            if (method_exists($this, $method)) {
-                $value = $this->$method($value, array_merge($this->data, $data), $this->relation);
-            } elseif (isset($this->type[$name])) {
-                // 类型转换
-                $value = $this->writeTransform($value, $this->type[$name]);
-            }
-        }
-
-        // 设置数据对象属性
-        $this->data[$name] = $value;
-        return $this;
-    }
-
-
-    /**
-     * 获取器 获取数据对象的值
-     * @access public
-     * @param string $name 名称
-     * @return mixed
-     * @throws InvalidArgumentException
-     */
-    public function getAttr($name)
-    {
-        try {
-            $value = $this->getData($name);
-        } catch (InvalidArgumentException $e) {
-            throw new InvalidArgumentException('property not exists:' . $this->class . '->' . $name);
-        }
-        return $value;
-    }
-
 
     /**
      * 转换当前模型对象为数组
@@ -167,38 +128,6 @@ class QueryFilters implements \JsonSerializable, \ArrayAccess
         return json_encode($this->toArray(), $options);
     }
 
-
-    /**
-     * 获取更新条件
-     * @access protected
-     * @return mixed
-     */
-    protected function getWhere()
-    {
-        // 删除条件
-        $pk = $this->getPk();
-
-        if (is_string($pk) && isset($this->data[$pk])) {
-            $where = [$pk => $this->data[$pk]];
-        } elseif (!empty($this->updateWhere)) {
-            $where = $this->updateWhere;
-        } else {
-            $where = null;
-        }
-        return $where;
-    }
-
-
-    /**
-     * 返回模型的错误信息
-     * @access public
-     * @return string|array
-     */
-    public function getError()
-    {
-        return $this->error;
-    }
-
     /**
      * 修改器 设置数据对象的值
      * @access public
@@ -217,9 +146,9 @@ class QueryFilters implements \JsonSerializable, \ArrayAccess
      * @param string $name 名称
      * @return mixed
      */
-    public function __get($name)
+    public function __get(string $name)
     {
-        return $this->getAttr($name);
+        return $this->getData($name);
     }
 
     /**
@@ -231,10 +160,10 @@ class QueryFilters implements \JsonSerializable, \ArrayAccess
     public function __isset($name)
     {
         try {
-            if (array_key_exists($name, $this->data) || array_key_exists($name, $this->relation)) {
+            if (array_key_exists($name, $this->data)) {
                 return true;
             } else {
-                $this->getAttr($name);
+                $this->getData($name);
                 return true;
             }
         } catch (InvalidArgumentException $e) {
@@ -249,7 +178,7 @@ class QueryFilters implements \JsonSerializable, \ArrayAccess
      * @param string $name 名称
      * @return void
      */
-    public function __unset($name)
+    public function __unset(string $name)
     {
         unset($this->data[$name]);
     }
@@ -268,7 +197,7 @@ class QueryFilters implements \JsonSerializable, \ArrayAccess
     // ArrayAccess
     public function offsetSet($name, $value)
     {
-        $this->setAttr($name, $value);
+        $this->setFilter($name, $value);
     }
 
     public function offsetExists($name): bool
@@ -283,7 +212,7 @@ class QueryFilters implements \JsonSerializable, \ArrayAccess
 
     public function offsetGet($name)
     {
-        return $this->getAttr($name);
+        return $this->setFilter($name);
     }
 
     /**
